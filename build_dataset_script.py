2e20f70678d940eec9d21fa5c182e6512902969a
luca96 <luca.anzalone.dev@gmail.com>
2023-07-13 15:37:31 +0200

    added notebooks

--- build_dataset_script.py
+++ build_dataset_script.py
@@ -31,124 +31,124 @@ def get_files(src: str, dst: str, should_split=False) -> list:
 
 
 def from_h5_to_npz(args: tuple):
-        id, file_name, src, dst, split, normalize, should_sum = args
-        dtype = np.float32
+    id, file_name, src, dst, split, normalize, should_sum = args
+    dtype = np.float32
 
-        should_split = split > 0.0
-        path = os.path.join(src, file_name)
-        print(f'[{id}] processing "{file_name}"..')
+    should_split = split > 0.0
+    path = os.path.join(src, file_name)
+    print(f'[{id}] processing "{file_name}"..')
 
-        # determine label
-        if 'h125' in file_name:
-            mass = 1
-            label = 1
+    # determine label
+    if 'h125' in file_name:
+        mass = 1
+        label = 1
 
-        elif 'h200' in file_name:
-            mass = 2
-            label = 1
+    elif 'h200' in file_name:
+        mass = 2
+        label = 1
 
-        elif 'h300' in file_name:
-            mass = 3
-            label = 1
+    elif 'h300' in file_name:
+        mass = 3
+        label = 1
 
-        elif 'h400' in file_name:
-            mass = 2 + 2
-            label = 1
+    elif 'h400' in file_name:
+        mass = 2 + 2
+        label = 1
 
-        elif 'h700' in file_name:
-            mass = 3 + 2
-            label = 1
+    elif 'h700' in file_name:
+        mass = 3 + 2
+        label = 1
 
-        elif 'h1000' in file_name:
-            mass = 4 + 2
-            label = 1
+    elif 'h1000' in file_name:
+        mass = 4 + 2
+        label = 1
 
-        elif 'svj' in file_name:
-            label = 2
+    elif 'svj' in file_name:
+        label = 2
 
-            # file_name has the format "svj_[mass]_[id]_[n].h5"
-            part = file_name.split('_')[1]
+        # file_name has the format "svj_[mass]_[id]_[n].h5"
+        part = file_name.split('_')[1]
 
-            # TODO: should change mass label for SVJ; these are still unique if paired with class label
-            mass = {2100: 5, 3100: 6, 4100: 7}[int(part)]
+        # TODO: should change mass label for SVJ; these are still unique if paired with class label
+        mass = {2100: 5, 3100: 6, 4100: 7}[int(part)]
+    else:
+        mass = 0
+        label = 0
+
+    # each file contains N 286x360 images of the plane (eta, phi)
+    with h5py.File(path, 'r') as file:
+        # inner-tracker image (with pile-up correction)
+        image_trk = np.array(file.get('ImageTrk_PUcorr'), dtype=dtype)
+
+        # ECAL image
+        image_ecal = np.array(file.get('ImageECAL'), dtype=dtype)
+
+        # HCAL image
+        image_hcal = np.array(file.get('ImageHCAL'), dtype=dtype)
+
+        # number of tracks
+        tracks = np.array(file.get('NTrk_PUcorr'), dtype=dtype)
+
+        # stack the three images to form 3-channel images
+        # shape: (N, 286, 360, 3)
+        images = np.stack([image_trk, image_ecal, image_hcal], axis=-1).astype(dtype)
+
+        # transpose to have (phi, eta) instead of (eta, phi)
+        # shape: (N, 360, 286, 3)
+        images = np.transpose(images, axes=[0, 2, 1, 3])
+        labels = np.ones(len(images), dtype=dtype) * float(label)
+        masses = np.ones(len(images), dtype=dtype) * float(mass)
+
+        # pre-processing
+        # down-sample (by 5x) and normalize images (to sum 1)
+        with tf.device('cpu'):
+            x = tf.nn.depthwise_conv2d(images, filter=tf.ones((5, 5, 3, 1)),
+                                       strides=[1, 5, 5, 1], padding='SAME')
+            if should_sum:
+                x = tf.reduce_sum(x, axis=-1, keepdims=True)
+
+            if normalize:
+                x /= tf.reduce_sum(x, axis=[1, 2, 3], keepdims=True)
+
+            images = np.array(x, dtype=dtype)
+
+        # training-validation split
+        if should_split:
+            x_train, x_valid, \
+            y_train, y_valid, \
+            t_train, t_valid, \
+            m_train, m_valid = train_test_split(images, labels, tracks, masses,
+                                                test_size=split, random_state=42)
+            # save
+            save_path_train = os.path.join(dst, 'train', file_name)
+            save_path_valid = os.path.join(dst, 'valid', file_name)
+
+            save_path_train, _ = os.path.splitext(save_path_train)  # remove .h5 extension
+            save_path_valid, _ = os.path.splitext(save_path_valid)
+
+            np.savez_compressed(save_path_train, images=x_train, labels=y_train,
+                                masses=m_train, n_tracks=t_train)
+
+            np.savez_compressed(save_path_valid, images=x_valid, labels=y_valid,
+                                masses=m_valid, n_tracks=t_valid)
+
+            print(f'  [{id}] saved at "{save_path_train}.npz"')
+            print(f'  [{id}] saved at "{save_path_valid}.npz"')
+
+            del x_train, x_valid, y_train, y_valid
+            gc.collect()
         else:
-            mass = 0
-            label = 0
+            save_path = os.path.join(dst, file_name)
+            save_path, _ = os.path.splitext(save_path)
 
-        # each file contains N 286x360 images of the plane (eta, phi)
-        with h5py.File(path, 'r') as file:
-            # inner-tracker image (with pile-up correction)
-            image_trk = np.array(file.get('ImageTrk_PUcorr'), dtype=dtype)
+            np.savez_compressed(save_path, images=images, labels=labels, masses=masses,
+                                n_tracks=tracks)
 
-            # ECAL image
-            image_ecal = np.array(file.get('ImageECAL'), dtype=dtype)
+            print(f'  [{id}] saved at "{save_path}.npz"')
 
-            # HCAL image
-            image_hcal = np.array(file.get('ImageHCAL'), dtype=dtype)
-
-            # number of tracks
-            tracks = np.array(file.get('NTrk_PUcorr'), dtype=dtype)
-
-            # stack the three images to form 3-channel images
-            # shape: (N, 286, 360, 3)
-            images = np.stack([image_trk, image_ecal, image_hcal], axis=-1).astype(dtype)
-
-            # transpose to have (phi, eta) instead of (eta, phi)
-            # shape: (N, 360, 286, 3)
-            images = np.transpose(images, axes=[0, 2, 1, 3])
-            labels = np.ones(len(images), dtype=dtype) * float(label)
-            masses = np.ones(len(images), dtype=dtype) * float(mass)
-
-            # pre-processing
-            # down-sample (by 5x) and normalize images (to sum 1)
-            with tf.device('cpu'):
-                x = tf.nn.depthwise_conv2d(images, filter=tf.ones((5, 5, 3, 1)),
-                                           strides=[1, 5, 5, 1], padding='SAME')
-                if should_sum:
-                    x = tf.reduce_sum(x, axis=-1, keepdims=True)
-
-                if normalize:
-                    x /= tf.reduce_sum(x, axis=[1, 2, 3], keepdims=True)
-
-                images = np.array(x, dtype=dtype)
-
-            # training-validation split
-            if should_split:
-                x_train, x_valid, \
-                y_train, y_valid, \
-                t_train, t_valid, \
-                m_train, m_valid = train_test_split(images, labels, tracks, masses,
-                                                    test_size=split, random_state=42)
-                # save
-                save_path_train = os.path.join(dst, 'train', file_name)
-                save_path_valid = os.path.join(dst, 'valid', file_name)
-
-                save_path_train, _ = os.path.splitext(save_path_train)  # remove .h5 extension
-                save_path_valid, _ = os.path.splitext(save_path_valid)
-
-                np.savez_compressed(save_path_train, images=x_train, labels=y_train,
-                                    masses=m_train, n_tracks=t_train)
-
-                np.savez_compressed(save_path_valid, images=x_valid, labels=y_valid,
-                                    masses=m_valid, n_tracks=t_valid)
-
-                print(f'  [{id}] saved at "{save_path_train}.npz"')
-                print(f'  [{id}] saved at "{save_path_valid}.npz"')
-
-                del x_train, x_valid, y_train, y_valid
-                gc.collect()
-            else:
-                save_path = os.path.join(dst, file_name)
-                save_path, _ = os.path.splitext(save_path)
-
-                np.savez_compressed(save_path, images=images, labels=labels, masses=masses,
-                                    n_tracks=tracks)
-
-                print(f'  [{id}] saved at "{save_path}.npz"')
-
-        # cleanup
-        del file, image_trk, image_ecal, image_hcal, images, labels
-        gc.collect()
+    # cleanup
+    del file, image_trk, image_ecal, image_hcal, images, labels
+    gc.collect()
 
 
 if __name__ == '__main__':
